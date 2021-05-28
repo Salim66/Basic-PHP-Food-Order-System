46bc2311df4eb327bd4e468a5efa6a3edbbe0523
Salim66 <salimhasanriad8@gmail.com>
2021-05-28 23:53:45 +0600

    Fetch category data on table

--- admin/manage-category.php
+++ admin/manage-category.php
@@ -4,23 +4,87 @@
     <div class="wrapper">
         <h1>Manage Category</h1>
         <br>
+        <?php
+        // Check whether the session value has or not
+        if (isset($_SESSION['success'])) {
+            echo $_SESSION['success'];
+            unset($_SESSION['success']);
+        }
+
+        // Check whether the session value has or not
+        if (isset($_SESSION['error'])) {
+            echo $_SESSION['error'];
+            unset($_SESSION['error']);
+        }
+        ?>
         <br>
         <table>
             <tr class="thead">
                 <td>#</td>
-                <td>Full Name</td>
-                <td>Username</td>
+                <td>Title</td>
+                <td>Image</td>
+                <td>Featured</td>
+                <td>Active</td>
                 <td>Active</td>
             </tr>
-            <tr>
-                <td>1</td>
-                <td>Salim Hasan</td>
-                <td>salim66</td>
-                <td>
-                    <a class="btn btn-info" href="">Edit Admin</a>
-                    <a class="btn btn-danger" href="">Delete Admin</a>
-                </td>
-            </tr>
+            <?php
+            // Create SQL query for select all data from databaes tbl_admin tabla
+            $sql = "SELECT * FROM tbl_category";
+            // Execute the query
+            $res = $connection->query($sql);
+
+            // Count the query
+            $count = mysqli_num_rows($res);
+
+            // Check whether the value has or not
+            if ($count > 0) {
+                // Data available
+                // Fecth all data 
+                $i = 1;
+                while ($data = $res->fetch_assoc()) {
+            ?>
+                    <tr>
+                        <td><?php echo $i++; ?></td>
+                        <td><?php echo $data['title']; ?></td>
+                        <td>
+                            <?php
+                            // Check whether the image has or not
+                            if ($data['image_name'] != "") {
+                            ?>
+                                <img style="width: 50px; height: 50px;" src="<?php echo SITEURL; ?>admin/uploads/category/<?php echo $data['image_name'] ?>" alt="">
+                            <?php
+                            }
+                            ?>
+                        </td>
+                        <td>
+                            <?php
+                            if ($data['featured'] == 'Yes') {
+                            ?>
+                                <div class="success">Yes</div>
+                            <?php
+                            } else if ($data['featured'] == 'No') {
+                            ?>
+                                <div class="error">No</div>
+                            <?php
+                            }
+                            ?>
+                        </td>
+                        <td>
+                            <a class="btn btn-info" href="<?php echo SITEURL; ?>admin/edit-admin.php?id=<?php echo $data['id']; ?>">Edit Admin</a>
+                            <a class="btn btn-danger" href="<?php echo SITEURL; ?>admin/delete-admin.php?id=<?php echo $data['id']; ?>">Delete Admin</a>
+                        </td>
+                    </tr>
+                <?php
+                }
+            } else {
+                ?>
+                <tr>
+                    <td colspan="4" class="error">Data not found!</td>
+                </tr>
+            <?php
+            }
+
+            ?>
         </table>
     </div>
 </div>
