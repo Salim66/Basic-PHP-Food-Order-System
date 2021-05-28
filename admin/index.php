bc2d1907b6cbe16866d599ad80ee4fa522cf014a
Salim66 <salimhasanriad8@gmail.com>
2021-05-28 20:20:52 +0600

    Add admin dashboard template

---
+++ admin/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Food Order System - Dashboard</title>
+    <link type="text/css" rel="stylesheet" href="css/style.css">
+    </link>
+</head>
+
+<body>
+
+    <!-- Menu Section Starts -->
+    <div class="menu">
+        <div class="wrapper text-center">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="manage-admin.php">Admin</a></li>
+                <li><a href="manage-category.php">Category</a></li>
+                <li><a href="manage-food.php">Food</a></li>
+                <li><a href="manage-order.php">Order</a></li>
+                <li><a href="logout.php">Logout</a></li>
+            </ul>
+        </div>
+    </div>
+    <!-- Menu Section Ends -->
+    <!-- Main Content Section Starts -->
+    <div class="main-content">
+        <div class="wrapper">
+            <h1>DASHBOARD</h1>
+            <br>
+            <br>
+            <div class="col-4 text-center">
+                <h1>5</h1><br>
+                Categories
+            </div>
+            <div class="col-4 text-center">
+                <h1>5</h1><br>
+                Foods
+            </div>
+            <div class="col-4 text-center">
+                <h1>5</h1><br>
+                Orders
+            </div>
+            <div class="col-4 text-center">
+                <h1>5</h1><br>
+                Review Generate
+            </div>
+        </div>
+    </div>
+    <!-- Main Content Section Ends -->
+    <!-- Footer Section Starts -->
+    <div class="footer">
+        <div class="wrapper text-center">
+            <p>All rights reserved, Food House. Developed By - <a href="">Salim Hasan</a></p>
+        </div>
+    </div>
+    <!-- Footer Section Ends -->
+
+</body>
+
+</html>
