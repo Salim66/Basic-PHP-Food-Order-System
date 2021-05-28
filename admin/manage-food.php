3be6ebc911f0030d48262e9976c2b5545eee8c88
Salim66 <salimhasanriad8@gmail.com>
2021-05-28 20:40:32 +0600

    Admin table design and copy all others pages amd link menu bar

---
+++ admin/manage-food.php
@@ -0,0 +1,28 @@
+<?php require_once('layout/menu.php') ?>
+<!-- Main Content Section Starts -->
+<div class="main-content">
+    <div class="wrapper">
+        <h1>Manage Food</h1>
+        <br>
+        <br>
+        <table>
+            <tr class="thead">
+                <td>#</td>
+                <td>Full Name</td>
+                <td>Username</td>
+                <td>Active</td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>Salim Hasan</td>
+                <td>salim66</td>
+                <td>
+                    <a class="btn btn-info" href="">Edit Admin</a>
+                    <a class="btn btn-danger" href="">Delete Admin</a>
+                </td>
+            </tr>
+        </table>
+    </div>
+</div>
+<!-- Main Content Section Ends -->
+<?php require_once('layout/footer.php') ?>
