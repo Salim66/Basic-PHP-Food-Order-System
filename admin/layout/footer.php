95ecdc55fde1c5032d2f0fe103197415f727a8d9
Salim66 <salimhasanriad8@gmail.com>
2021-05-28 20:26:42 +0600

    Slicing admin dashboard

---
+++ admin/layout/footer.php
@@ -0,0 +1,11 @@
+ <!-- Footer Section Starts -->
+ <div class="footer">
+     <div class="wrapper text-center">
+         <p>All rights reserved, Food House. Developed By - <a href="">Salim Hasan</a></p>
+     </div>
+ </div>
+ <!-- Footer Section Ends -->
+
+ </body>
+
+ </html>
